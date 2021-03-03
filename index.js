fd274eb04ea2f2b0012b70382da9d6421a8a0ed5
Ilya <olovyannikov_barista@icloud.com>
2021-03-03 12:17:10 +0300

    Добавляет bodyParser

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-    res.end('Hello world!')
+    res.render('index')
+})
+
+app.post('/', (req, res) => {
+    const {city} = req.body
+
+    console.log(city)
+    res.render('index')
 })
 
 app.listen(3000, () => {
-    console.log('Server has started on port 3000...');
-});
+    console.log('Server has started on port 3000...')
+})
